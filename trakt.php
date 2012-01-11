e60811deca415291e567d34a1622d5fb25f893cc
Weston <wnielson@gmail.com>
2012-01-10 16:54:57 -0800

    Added POST and authentication support.  Some methods still need to be added.

--- trakt.php
+++ trakt.php
@@ -1,4 +1,52 @@
 <?php
+/**
+ * A simple class for accessing the Trakt API.  You can use it like so:
+ *
+ *  $trakt = new Trakt("You API Key");
+ *  $trakt->showSeasons("The Walking Dead", true);
+ *
+ * You can view the list of available API methods here: http://trakt.tv/api-docs
+ * To call a method, such as "search/movies", the ``Trakt`` class will respond
+ * to the corresponding method name "searchMovies".  So, in the above example, the
+ * following would work:
+ *
+ *  $trakt->searchMovies("28 Days Later");
+ *
+ * To call any methods that require authentication, you must first set the
+ * authentication data:
+ *
+ *    $trakt->setAuth("username", "password");
+ *
+ *
+ * Now the following will work:
+ *
+ *    $trakt->activityFriends();
+ *
+ *
+ * POST requests are also supported and behave in much the same way as GET requests.
+ * For example, to test your login credentials, you can do:
+ *
+ *    $trakt->accountTest("myusername", "mypassword");
+ *
+ *
+ * One important thing to note is that if one or more arguments to a POST-method are
+ * listed as "optional" in the API docs, you still need to include them when calling
+ * the method.  So, for example "/lists/add/" lists the "description" as optional, so
+ * here's how one might use this method:
+ *
+ *    $trakt->listsAdd("A Test List", "", "public");
+ *
+ * The order of the arguments is important and follows the order defined in the API
+ * docs, but if in doubt, check the source code below.  Also, for those methods that
+ * use POST and require authentication, you shouldn't supply the "username" and
+ * "password" as arguments, instead use the ``setAuth`` function described above.  So
+ * to add a friend via "/friends/add/", one should do:
+ *
+ *    $trakt->setAuth("myUsername", "myPassword");
+ *    $trakt->friendsAdd("myFriendsUsername");
+ *
+ */
+
 
 /**
  * Generate and return a slug for a given ``$phrase``.
@@ -13,19 +61,6 @@ function slugify($phrase)
     return $result;
 }
 
-/**
- * A simple class for accessing the Trakt API.  You can use it like so:
- *
- *  $trakt = new Trakt("You API Key");
- *  $trakt->showSeasons("The Walking Dead", true);
- *
- * You can view the list of available API methods here: http://trakt.tv/api-docs
- * To call a method, such as "search/movies", the ``Trakt`` class will respond
- * to the corresponding method name "searchMovies".  So, in the above example, the
- * following would work:
- *
- *  $trakt->searchMovies("28 Days Later");
- */
 
 class Trakt
 {
@@ -33,9 +68,22 @@ class Trakt
     public  $errNum = 0;
     public  $errMsg = '';
     
-    public  $trackHost = "http://api.trakt.tv";
+    public  $trackHost = "https://api.trakt.tv";
     
     private $urls = array(
+        /**
+         * Account methods
+         */
+        "/account/create/" => array(
+            array("name" => "username", "method" => "post"),
+            array("name" => "password", "method" => "post"),
+            array("name" => "email",    "method" => "post")
+        ),
+        "/account/test/" => array(
+            array("name" => "username", "method" => "post"),
+            array("name" => "password", "method" => "post")
+        ),
+    
         /**
          * Activity methods
          */
@@ -51,9 +99,11 @@ class Trakt
             array("name" => "actions",   "optional" => true),
             array("name" => "timestamp", "optional" => true)
         ),
-        /*
-        "/activity/friends.json/" => array().
-        */
+        "/activity/friends.json/" => array(
+            array("name" => "types",     "optional" => true),
+            array("name" => "actions",   "optional" => true),
+            array("name" => "timestamp", "optional" => true)
+        ),
         "/activity/movies.json/" => array(
             array("name" => "titleOrId", "convert"  => slugify),
             array("name" => "actions",   "optional" => true),
@@ -91,19 +141,54 @@ class Trakt
         
         /**
          * Friends methods
-         *     TODO: Add these
-        */
+         */
+        "/friends/add/" => array(
+            array("name" => "friend", "action" => "post")
+        ),
+        "/friends/all/" => null,
+        "/friends/approve/" => array(
+            array("name" => "friend", "action" => "post")
+        ),
+        "/friends/delete/" => array(
+            array("name" => "friend", "action" => "post")
+        ),
+        "/friends/deny/" => array(
+            array("name" => "friend", "action" => "post")
+        ),
+        "/friends/requests/" => null,
         
         /**
          * Genres methods
          */
-        "/genres/movies.json/" => array(),
-        "/genres/shows.json/" => array(),
+        "/genres/movies.json/" => null,
+        "/genres/shows.json/"  => null,
         
         /**
          * Lists methods
          *    TODO: Add these
          */
+        "/lists/add/" => array(
+            array("name" => "name",        "method" => "post"),
+            array("name" => "description", "method" => "post"), //"optional" => true),
+            array("name" => "privacy",     "method" => "post")
+        ),
+        "/lists/delete/" => array(
+            array("name" => "slug", "convert" => slugify, "method" => "post")
+        ),
+        "/lists/items/add/" => array(
+            array("name" => "slug", "convert" => slugify, "method" => "post"),
+            array("name" => "items", "method" => "post")
+        ),
+        "/lists/items/delete/" => array(
+            array("name" => "slug", "convert" => slugify, "method" => "post"),
+            array("name" => "items", "method" => "post")
+        ),
+        "/lists/update/" => array(
+            array("name" => "slug",        "method" => "post", "convert" => slugify),
+            array("name" => "name",        "method" => "post"),
+            array("name" => "description", "method" => "post"), //"optional" => true),
+            array("name" => "privacy",     "method" => "post")
+        ),
         
         /**
          * Movie methods
@@ -125,7 +210,7 @@ class Trakt
         /**
          * Movies methods
          */
-        "/movies/trending.json/" => array(),
+        "/movies/trending.json/" => null,
         
         /**
          * Search methods
@@ -189,7 +274,7 @@ class Trakt
         /**
          * Shows methods
          */
-        "/shows/trending.json/" => array(),
+        "/shows/trending.json/" => null,
         
         /**
          * User methods
@@ -267,23 +352,38 @@ class Trakt
     {
         $this->apiKey = $apiKey;
         $this->debug = $debug;
+        $this->clearAuth();
     }
     
     public function __call($method, $arguments)
     {
         $methodUrl = $this->getMethodUrl($method);
+        if (!array_key_exists($methodUrl, $this->urls)) {
+            // Try post instead
+            $methodUrl = $this->getMethodUrl($method, "");
+        }
+        
         if (array_key_exists($methodUrl, $this->urls)) {
             $url = $this->buildUrl($methodUrl);
+            $post = array();
+            
             foreach($arguments as $index => $arg) {
                 if (array_key_exists($index, $this->urls[$methodUrl])) {
-                    
                     $opts = $this->urls[$methodUrl][$index];
+                    
+                    // Determine how to represent this field
+                    $data = $arg;
                     if (array_key_exists("convert", $opts)) {
-                        $url .= $opts["convert"]($arg)."/";
+                        $data = $opts["convert"]($arg);
                     } else if (array_key_exists("optional", $opts) && $arg === true) {
-                        $url .= $opts["name"]."/";
+                        $data = $opts["name"];
+                    }
+                    
+                    // Is this a post field?
+                    if (array_key_exists("method", $opts) && $opts["method"] == "post") {
+                        $post[$opts["name"]] = $data;
                     } else {
-                        $url .= $arg."/";
+                        $url .= $data."/";
                     }
                 }
             }
@@ -293,18 +393,30 @@ class Trakt
                 printf("URL: %s\n", $url);
             }
             
-            return $this->getUrl($url);
+            return $this->getUrl($url, $post);
         }
         return false;
+    }
+    
+    public function clearAuth()
+    {
+        $this->username = null;
+        $this->password = null;
+    }
+    
+    public function setAuth($username, $password)
+    {
+        $this->username = $username;
+        $this->password = sha1($password);
     }
     
     /**
      * Given a string like "showSeason", returns "/show/season.json/"
      */
-    private function getMethodUrl($method, $format="json") {
+    private function getMethodUrl($method, $format=".json") {
         $method[0] = strtolower($method[0]);
         $func = create_function('$c', 'return "/" . strtolower($c[1]);');
-        return "/".preg_replace_callback('/([A-Z])/', $func, $method).".".$format."/";
+        return "/".preg_replace_callback('/([A-Z])/', $func, $method).$format."/";
     }
     
     /**
@@ -320,7 +432,7 @@ class Trakt
      * Query the ``$url`` and convert the JSON into an associative array.
      * If error are encountered, ``false`` is returned instead.
      */
-    private function getUrl($url)
+    private function getUrl($url, $post=null)
     {
         $ch = curl_init();
         
@@ -331,6 +443,19 @@ class Trakt
         curl_setopt($ch, CURLOPT_FAILONERROR, false); //trakt sends a 401 with 
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        
+        if ($this->username && $this->password) {
+            curl_setopt($ch, CURLOPT_USERPWD, $this->username.":".$this->password);
+        }
+        
+        if ($post) {
+            $data = json_encode($post);
+            if ($this->debug) {
+                var_dump($data);
+            }
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
         
         $buffer = curl_exec($ch);
         
